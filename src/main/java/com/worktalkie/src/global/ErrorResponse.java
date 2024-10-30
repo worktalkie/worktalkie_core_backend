44fcebfe2d42b61ad5350766c2573fc7facf9119
Hyunstone <hyun104467@naver.com>
2024-10-31 00:57:45 +0900

    feat: error response 도입

---
+++ src/main/java/com/worktalkie/src/global/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.worktalkie.src.global;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.worktalkie.src.global.error.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code"})
+public class ErrorResponse {
+    private Boolean isSuccess;
+    private String code;
+    private String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.isSuccess = errorCode.isSuccess();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+}
--- src/main/java/com/worktalkie/src/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/worktalkie/src/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.worktalkie.src.global.error;
 
-import com.worktalkie.src.global.BaseResponse;
+import com.worktalkie.src.global.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +12,7 @@ import java.util.Optional;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
-    public BaseResponse<ErrorCode> BaseException(BaseException error) {
+    public ErrorResponse baseExceptionHandler(BaseException error) {
         ErrorCode errorCode;
 
         try {
@@ -22,6 +22,6 @@ public class GlobalExceptionHandler {
             log.error("error: {}", e.getMessage());
             errorCode = ErrorCode.INTERNAL_SERVER_ERROR;  // 예외 발생 시 기본 에러 코드로 처리
         }
-        return new BaseResponse<>(errorCode);
+        return new ErrorResponse(errorCode);
     }
 }
