a15fba6513f4dc70816774a79ac92c1073a35d64
Hyunstone <hyun104467@naver.com>
2024-11-15 15:04:49 +0900

    test: 카테고리 매거진 조회 api

--- src/test/java/com/worktalkie/src/magazine/e2e/MagazineE2ETest.java
+++ src/test/java/com/worktalkie/src/magazine/e2e/MagazineE2ETest.java
@@ -14,7 +14,6 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.worktalkie.src.base.IntegrationTest;
-import com.worktalkie.src.global.BaseResponse;
 import com.worktalkie.src.magazine.code.MagazineCategory;
 import com.worktalkie.src.magazine.dto.MagazineResponse;
 import com.worktalkie.src.magazine.entity.Magazine;
@@ -28,7 +27,7 @@ class MagazineE2ETest extends IntegrationTest {
 
 	@DisplayName("매거진 목록을 페이징 조회한다")
 	@Test
-	void 카테고리_매거진_목록_조회_테스트() throws Exception {
+	void 매거진_목록_조회_테스트() throws Exception {
 		// given
 		Magazine magazine1 = Magazine.builder()
 			.id(1L)
@@ -42,7 +41,7 @@ class MagazineE2ETest extends IntegrationTest {
 			.id(2L)
 			.title("매거진2")
 			.description("좋은 매거진이다")
-			.category(MagazineCategory.IT)
+			.category(MagazineCategory.ETC)
 			.createdBy("매니저")
 			.build();
 
@@ -50,7 +49,7 @@ class MagazineE2ETest extends IntegrationTest {
 			.id(3L)
 			.title("매거진3")
 			.description("좋은 매거진이다11")
-			.category(MagazineCategory.IT)
+			.category(MagazineCategory.ALL)
 			.createdBy("매니정")
 			.build();
 
@@ -75,4 +74,64 @@ class MagazineE2ETest extends IntegrationTest {
 			.extracting(MagazineResponse.PagingMagazinesDto::getId)
 			.containsExactly(1L, 2L, 3L);
 	}
+
+	@DisplayName("IT 매거진 목록을 페이징 조회한다")
+	@Test
+	void IT_카테고리_매거진_목록_조회_테스트() throws Exception {
+		// given
+		Magazine magazine1 = Magazine.builder()
+			.id(1L)
+			.title("매거진1")
+			.description("좋은 매거진이다")
+			.category(MagazineCategory.IT)
+			.createdBy("매니정")
+			.build();
+
+		Magazine magazine2 = Magazine.builder()
+			.id(2L)
+			.title("매거진2")
+			.description("좋은 매거진이다")
+			.category(MagazineCategory.ETC)
+			.createdBy("매니저")
+			.build();
+
+		Magazine magazine3 = Magazine.builder()
+			.id(3L)
+			.title("매거진3")
+			.description("좋은 매거진이다11")
+			.category(MagazineCategory.ALL)
+			.createdBy("매니정")
+			.build();
+
+		Magazine magazine4 = Magazine.builder()
+			.id(4L)
+			.title("매거진4")
+			.description("좋은 매거진이다_ITIT")
+			.category(MagazineCategory.IT)
+			.createdBy("매니정")
+			.build();
+
+		magazineRepository.saveAll(List.of(magazine1, magazine2, magazine3, magazine4));
+
+		// when
+		int cursor = 0;
+		int limit = 3;
+		final MvcResult result = mockMvc.perform(get(
+				"/api/magazines?" + "category=" + MagazineCategory.IT + "&cursor=" + cursor + "&limit=" + limit))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andReturn();
+
+		// then
+		List<MagazineResponse.PagingMagazinesDto> magazinesDtos = JsonUtils.parseResponse(
+			result.getResponse(),
+			new TypeReference<>() {}
+		);
+
+		Assertions.assertThat(magazinesDtos)
+			.hasSize(2)
+			.extracting(MagazineResponse.PagingMagazinesDto::getId)
+			.containsExactly(1L, 4L);
+	}
+
 }
